4f88821c214db815b56e14a362fce4c42cf45fec
zubair <zubair.hassan@alchemative.com>
2020-03-01 02:47:20 +0500

    Updates

---
+++ precache-manifest.8b63e8c7df8c4e9c6ec6478862541481.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74bde1b9d7796a934522bd5ccb7ade31",
+    "url": "/zubairhassan652/portfolio/index.html"
+  },
+  {
+    "revision": "4535054cbc3e4d89776a",
+    "url": "/zubairhassan652/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "06ff18d69c1165883564",
+    "url": "/zubairhassan652/portfolio/static/js/2.a2917b7d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/zubairhassan652/portfolio/static/js/2.a2917b7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4535054cbc3e4d89776a",
+    "url": "/zubairhassan652/portfolio/static/js/main.fdc92278.chunk.js"
+  },
+  {
+    "revision": "840e9d0671001d4ef16c",
+    "url": "/zubairhassan652/portfolio/static/js/runtime-main.d7db33d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zubairhassan652/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
