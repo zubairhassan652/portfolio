6cb302c326b84512a9a9c48e994e4f4739afb3f7
Zubair Hassan <zubair.hassan@arbisoft.com>
2020-12-09 11:08:30 +0500

    simple webset

--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 # Create your views here.
 
 def hello(request):
-    return render(request, 'build/index.html')
+
+    # return render(request, 'build/index.html')
+    return HttpResponse('Hello World')
