30c24e4a9d55a1335ec2248f151d69acb6e0eae1
Zubair Hassan <zubair.hassan@arbisoft.com>
2020-12-11 18:32:08 +0500

    about text tweak

--- frontend/src/components/sections/about.js
+++ frontend/src/components/sections/about.js
@@ -13,11 +13,9 @@ export default function About() {
                     </h4>
                     <h5>
                         I'm a full-stack engineer with a strong preference for back-end development. 
-                        I pride myself on writing clean, readable, testable and extensible code.<br /><br />
+                        I pride myself on writing clean, readable, testable and extensible code.
                         Outside of work, I am always looking to learn new frameworks, libraries and programming languages.
-                        To help me truly understand these technologies, I use them in my personal projects. 
-                        I also maintain a technical blog where I document my approach to solving technical problems I've encountered throughout my career.
-                    
+                        To help me truly understand these technologies, I use them in my personal projects.
                     </h5>
                 </div>
             </div>
