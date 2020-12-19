2229ef2459bfb562b4c23f6047e304c467532ee1
Zubair Hassan <zubair.hassan@arbisoft.com>
2020-12-19 23:12:25 +0500

    react portfolio updated structure

--- backend/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse
-# Create your views here.
-
-def hello(request):
-
-    return render(request, 'build/index.html')
-    # return HttpResponse('Hello World')
